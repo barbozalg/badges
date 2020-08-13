6439224af2785da47a01cd971cf983ecbbab1133
Luis G Barboza <barbozalg@outlook.com>
2020-08-13 15:31:49 -0600

    Adding access
    Showing the scope of the variables

--- java/apprentice/access_modifiers/src/main/java/barbozalg/access_modifiers/Main.java
+++ java/apprentice/access_modifiers/src/main/java/barbozalg/access_modifiers/Main.java
@@ -1,8 +1,10 @@
 package barbozalg.access_modifiers;
 
+import barbozalg.other_package.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
@@ -14,21 +16,26 @@ public class Main implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         scope test = new scope();
-        System.out.println("----- Read From Another class -----");
+        new Auxiliar(test);
+        System.out.println("----- Read From Another Package -----");
         // System.out.println(test.a);
         System.out.println("private int a: Private Read Access Restrictions");
-        System.out.println("int b (Default): " + test.b);
-        System.out.println("protected int c: " + test.c);
+        // System.out.println("int b (Default): " + test.b);
+        System.out.println("int b (Default): Private Read Access Restrictions");
+        // System.out.println("protected int c: " + test.c);
+        System.out.println("protected int c: Private Read Access Restrictions");
         System.out.println("public int d: " + test.d + "\n");
 
-        System.out.println("----- Write From Another class -----");
+        System.out.println("----- Write From Another Package -----");
         // System.out.println(test.a);
         System.out.println("private int a: Private Write Access Restrictions");
-        test.b = 6;
-        System.out.println("int b (Default): " + test.b);
-        test.c = 7;
-        System.out.println("protected int c: " + test.c);
-        test.d = 8;
+        //test.b = 6;
+        //System.out.println("int b (Default): " + test.b);
+        System.out.println("int b (Default): Private Read Access Restrictions");
+        //test.c = 7;
+        //System.out.println("protected int c: " + test.c);
+        System.out.println("protected int c: Private Read Access Restrictions");
+        test.d = 9;
         System.out.println("public int d: " + test.d + "\n");
     }
 
