4a88465dbb049b8ac2331754dc31bebf68158177
Luis G Barboza <barbozalg@outlook.com>
2020-08-13 12:18:49 -0600

    file sort
    Creating file sort app

---
+++ java/apprentice/file_sort/src/main/java/barbozalg/file_sort/Main.java
@@ -0,0 +1,41 @@
+/**
+ * @author barbozalg on Jun 29 2020.
+ *
+ * Assignment:
+ * - Write an application to read a file with 10k lines of text, and output another file with the lines in sorted order (sample file)
+ * - Write an application to read a file with 10k lines of text, and output another file with the lines in reverse sorted order
+ * - Write your own enum type. Describe when you would use it.
+ */
+
+package barbozalg.file_sort;
+
+import java.util.Scanner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Main implements CommandLineRunner {
+
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\n\n\nEnter the input file name:");
+        String file_name = scanner.nextLine();
+        System.out.println("\nEnter the order type:");
+        System.out.println(ANSI_YELLOW + "1" + ANSI_WHITE + ") Alphabetical Order [A-Z]");
+        System.out.println(ANSI_YELLOW + "2" + ANSI_WHITE + ") Reverse Order [Z-A]");
+        int reverse = scanner.nextInt();
+        Order order = (reverse == 2)? Order.REVERSE : Order.ORDERED;
+        FileSort.sort_file(file_name, order);
+    }
+
+}
---
+++ java/apprentice/file_sort/src/test/java/barbozalg/file_sort/FileSortApplicationTests.java
@@ -0,0 +1,13 @@
+package barbozalg.file_sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
