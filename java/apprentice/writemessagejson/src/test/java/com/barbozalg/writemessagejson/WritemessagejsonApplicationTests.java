728fa56dce3369b3794b4b2244ed5719e681c07d
Luis G Barboza <barbozalg@outlook.com>
2020-11-13 17:12:23 -0700

    Creating a new writemessagejson from writemessage
     I created a new project based on writemesage but this time create a JSON file instead a XML

---
+++ java/apprentice/writemessagejson/src/test/java/com/barbozalg/writemessagejson/WritemessagejsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbozalg.writemessagejson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritemessagejsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
