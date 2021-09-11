051747f2d13228e3519db0f22aa39d59f4b8a8b2
Luis Barboza <46587848+barbozalg@users.noreply.github.com>
2021-09-10 19:12:48 -0600

    New assignment (#19)
    Write code that declares, implements and/or extends interfaces.

---
+++ java/journeyman/interfaceclass/src/main/java/com/barbozalg/interfaceclass/Variations.java
@@ -0,0 +1,14 @@
+/**
+ * @author barbozalg on Sep 10 2021.
+ */
+
+package com.barbozalg.interfaceclass;
+
+
+public interface Variations {
+
+    void assignsValue(double x);
+
+    void discount(double t);
+
+}
---
+++ java/journeyman/interfaceclass/src/test/java/com/barbozalg/interfaceclass/InterfaceclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbozalg.interfaceclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
